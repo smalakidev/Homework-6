649d0d2fab6dc5b8cbc3a1cde13ac627b75823de
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-02-01 19:25:58 -0500

    Added some HTMl and jS

---
+++ index.js
@@ -0,0 +1,5 @@
+// enter city name in input
+//hit search btn 
+// on click event, which trigers a function
+//      grab value from input, store it into a var
+//      pass that var as anargument to a funciton;
