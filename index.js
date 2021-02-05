53dbfa979dff67e7a4ae840386d58489ab0440da
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-02-04 21:29:35 -0500

    made changes to JS

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+$(document).ready(function()  {
+
+//jquery used here to grab #search-input
+let searchCity = $("#search-input");
+
+//.ON replaces addEvent Listener in this case and brings
+//the click functionality 
+searchCity.on("click" , function(event) {
+console.log(event);
+};
+
+//Current date and time with Moment.js
+moment().format('MMMM Do YYYY, h:mm:ss a');
+
+const searchCity = $("#search-input").val();
 // enter city name in input
 //hit search btn 
 // on click event, which trigers a function
 //      grab value from input, store it into a var
-//      pass that var as anargument to a funciton;
+//      pass that var as an argument to a funciton;
+
+
+
+
+
+
+
+} );
+
