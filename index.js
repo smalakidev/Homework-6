ab8ae6e12c0048e2826c977ba19bc158ab8e2111
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-02-14 17:20:46 -0500

    Modified HTML and JS

--- index.js
+++ index.js
@@ -1,18 +1,90 @@
+//Require tells the file that there's a special library and 
+//we will import it 
+// const moment = require("moment");
+//Setting a current date for the webpage. I grabbed my HTML with $
+let currentDate = $(".Date").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+//Document.ready makes sure that our HTML runs on JavaScript
 $(document).ready(function()  {
 
-//jquery used here to grab #search-input
-let searchCity = $("#search-input");
+
+
+var APIKEY ="7340c61d1124828c3bc01eb7d3fb07cb"
+//jquery is used to grab the search button 
+let searchbtn = $("#search-btn");
 
 //.ON replaces addEvent Listener in this case and brings
-//the click functionality 
-searchCity.on("click" , function(event) {
-console.log(event);
+//the click functionality for the search button
+searchbtn.on("click" , function(event) {
+    event.preventDefault();
+//grab value from input (what the users typed)
+        const searchCity = $("#search-input").val();
+        if (searchCity=== "") {
+            alert("Please enter a town or City in the search bar")
+        }else{
+
+        console.log(searchCity)
+            //call function to make ajax call
+            //pass argument searchCity
+            getCurrentWeather(searchCity);
+        }
+});
+
+
+//declares argument
+function getCurrentWeather(city){
+        console.log(city)
+    //ajax call
+    $.ajax({
+        type: "GET",
+        url:"http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKEY + "&units=imperial"
+    })
+        .then((res)=>{
+            console.log(res)
+    //store city in local storage if not there already
+    //clear previous content
+
+    //create html for current weather
+           //car. Here we are  literally creating HTML Content with JavaScript
+           var card = $("<div>").addClass("currentCard");
+        //city name
+            var cityName = $("<h2>").text(city);
+            //wind
+            var wind = $("<h3>").text("WIND " + res.wind.speed + " MPH" );
+            //huminidty
+            var humidity = $("<h3>").text("Humidity " +res.main.humidity + "%");
+            //temperature
+            var temperature = $("<h3>").text("temperature " + res.main.temp + "F");
+            //img
+
+            card.append(cityName, wind, humidity , temperature)
+            $(".currentWeather").append(card)
+        
+
+
+    //call next ajaxs
+    //      - to get forecast
+    //      - to get uv index
+
+
+
+
+
+
+
+        })
 };
 
-//Current date and time with Moment.js
-moment().format('MMMM Do YYYY, h:mm:ss a');
 
-const searchCity = $("#search-input").val();
+
+// function.localStorage()
+
+//Get from local storage
+//submit to local storage
+//Get Weather API
+//Current date and time with Moment.js
+
+
+
 // enter city name in input
 //hit search btn 
 // on click event, which trigers a function
